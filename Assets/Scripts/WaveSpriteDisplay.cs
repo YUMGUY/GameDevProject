8030549d886f61cc4e9e74ac040138dadc083834
SeanJKelly <seanjosephkelly14@hotmail.com>
2023-01-30 13:01:04 -0600

    Removed empty wave sprite display object
    Changed it for a supported sprite display object from unity. This may be undone however.

---
+++ Assets/Scripts/WaveSpriteDisplay.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpriteDisplay : MonoBehaviour
+{
+    public void displayWaveSprite()
+    {
+
+    }
+}
