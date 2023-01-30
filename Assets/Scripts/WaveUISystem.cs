94fa3575e72dded54da3369c6b693b54ded778f5
SeanJKelly <seanjosephkelly14@hotmail.com>
2023-01-30 12:09:04 -0600

    Removed Wave System
    Transferred the Wave system script to the Canvas

--- Assets/Scripts/WaveUISystem.cs
+++ Assets/Scripts/WaveUISystem.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // for interfacing with the system's subobjects (timer, wave number display, ect)
 
 public class WaveUISystem : MonoBehaviour
 {
+    public TextMeshProUGUI timer;
+    public TextMeshProUGUI waveCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        waveCounter.GetComponent<WaveCounter>().setWave(5);
     }
 }
