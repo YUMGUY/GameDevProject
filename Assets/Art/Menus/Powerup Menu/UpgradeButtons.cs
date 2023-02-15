405cfc487d4e9dccaad86b3e2d3c6205547638f4
Alter33 <jnuckolls@tamu.edu>
2023-02-15 14:56:22 -0600

    Upgrade menu delayed
    Upgrade menu commented out, code can be re-implemented as needed

--- Assets/Art/Menus/Powerup Menu/UpgradeButtons.cs
+++ Assets/Art/Menus/Powerup Menu/UpgradeButtons.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UpgradeButtons : MonoBehaviour
-{
-    [SerializeField] Image icon;
-	
-	public void Set(UpgradeData upgradeData)
-	{
-		icon.sprite = upgradeData.icon;
-	}
-}
+// public class UpgradeButtons : MonoBehaviour
+ // {
+    // [SerializeField] Image icon;
+	 // public void Set(UpgradeData upgradeData)
+	 // {
+		 // icon.sprite = upgradeData.icon;
+		
+	 // }
+// }
--- Assets/Art/Menus/Powerup Menu/UpgradeMenu.cs
+++ Assets/Art/Menus/Powerup Menu/UpgradeMenu.cs
@@ -1,20 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public enum UpgradeType
-{
-	WeaponUpgrade,
-	ItemUpgrade,
-	WeaponUnlock,
-	ItemUnlock
-}
+// public class UpgradeMenu : MonoBehaviour
+// {
+    // public static bool GameIsPaused = false;
+	
+	// public GameObject upgradeMenuUI;
 
-[CreateAssetMenu]
+    // // Update is called once per frame
+    // void Update()
+    // {
+		// /*Input for level-up goes in if once determined*/
+        // if (Input.GetKeyDown(KeyCode.Escape))
+		// {
+			// if(GameIsPaused)
+			// {
+				// Resume();
+			// } else
+			// {
+				// Pause();
+			// }
+		// }
+	// }
+	
+	// void Pause ()
+	// {
+		// upgradeMenuUI.SetActive(true);
+		// Time.timeScale = 0f;
+		// GameIsPaused = true;
+	// }
+	
+	// public void Upgrade1 ()
+	// {
+		// Debug.Log("Upgrade1");
+		// upgradeMenuUI.SetActive(false);
+		// Time.timeScale = 1f;
+		// GameIsPaused = false;
+	// }
+	
+	// public void Upgrade2()
+	// {
+		// Debug.Log("Upgrade2");
+		// upgradeMenuUI.SetActive(false);
+		// Time.timeScale = 1f;
+		// GameIsPaused = false;
+	// }
+	
+	// public void Upgrade3()
+	// {
+		// Debug.Log("Upgrade3");
+		// upgradeMenuUI.SetActive(false);
+		// Time.timeScale = 1f;
+		// GameIsPaused = false;
+	// }
+// }
+//
+// public enum UpgradeType
+// {
+	// WeaponUpgrade,
+	// ItemUpgrade,
+	// WeaponUnlock,
+	// ItemUnlock
+// }
 
-public class UpgradeMenu : ScriptableObject
-{
-    public UpgradeType upgradeType;
-	public string Name;
-	public Sprite icon;
-}
+// [CreateAssetMenu]
+
+// public class UpgradeMenu : ScriptableObject
+// {
+    // public UpgradeType upgradeType;
+	// public string Name;
+	// public Sprite icon;
+// }
