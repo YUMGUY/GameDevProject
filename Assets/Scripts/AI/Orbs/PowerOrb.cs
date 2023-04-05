4f8c1b22694793372e25d7211ad0d1b0125eeece
Carlos Alvarez del Castillo Saleh <carlosadelcs@gmail.com>
2023-04-05 02:17:32 -0500

    Improved power orb logic
    Left collider in place but movement is now handled by the base again
    Power orb speed interpolation can also be done using custom curves now so that's nice

---
+++ Assets/Scripts/AI/Orbs/PowerOrb.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class <c>PowerOrb</c> is a collectable that gravitates towards the collector once they
+/// are within a certain range of each other.
+/// </summary>
+public class PowerOrb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    /// <summary>
+    /// Power orb movment is handled by the CollectOrbs script attached to the BaseCore object.
+    /// </summary>
+    void Update()
+    {
+    }
+
+    /// <summary>
+    /// Orbs are collected upon collision. This function contains the logic that should 
+    /// run when an orb is collected
+    /// </summary>
+    /// <param name="collision">The other Collider involved in this collision</param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        MultiTag collisionTags = collision.GetComponent<MultiTag>();
+        if(collisionTags != null)
+        {
+            // case: orb is collected by player
+            if(collisionTags.ContainsTag("Player"))
+            {
+                // gets the script that contains a reference to the core data
+                CollectOrbs collectOrbs = collision.GetComponent<CollectOrbs>();
+                if(collectOrbs != null)
+                {
+                    // gets the core data
+                    CoreData playerCoreData = collectOrbs.coreData;
+
+                    if (playerCoreData != null)
+                    {
+                        // increments player's collected energy
+                        playerCoreData.addEnergy(1.0f);
+                    }
+                }
+                DestroyObject();
+            }
+
+            // case: orb is collected by orb hunter enemy
+            else if (collisionTags.ContainsTag("OrbHunter"))
+            {
+                OrbHunter orbHunter = collision.GetComponent<OrbHunter>();
+                if(orbHunter != null)
+                {
+                    orbHunter.IncrementOrbs();
+                }
+                DestroyObject();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Contains the logic that should run when the power orb is destroyed
+    /// </summary>
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
