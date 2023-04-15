0f781df4821a884fad57dd88993c08febd319fb1
Carlos Alvarez del Castillo Saleh <carlosadelcs@gmail.com>
2023-04-15 15:58:15 -0500

    Create big warning animation

--- Assets/AlertSystem/Alert.cs
+++ Assets/AlertSystem/Alert.cs
@@ -10,38 +10,55 @@ public class Alert : MonoBehaviour
     /// <summary>
     /// The UI element that holds the text that will be displayed to the player.
     /// </summary>
-    [SerializeField] private TextMeshProUGUI alertTextUI;
+    [SerializeField] private TextMeshProUGUI notificationAlertTextUI;
 
     [Header("Animation")]
     /// <summary>
     /// The UI transition animation used to make the notification appear (ex: fade in,
     /// slide in, etc). Plays in reverse when the notification is removed. 
     /// </summary>
-    [SerializeField] private Animator alertAnimation;
+    [SerializeField] private Animator notificationAlertAnimation;
+
+    /// <summary>
+    /// The UI animation used to display the big warning message
+    /// </summary>
+    [SerializeField] private Animator bigWarningAlertAnimation;
 
     /// <summary>
     /// Used to track when an alert is being displayed. Used mainly to prevent bugs 
-    /// from trying to play the display/remove alert animations too many times. While 
-    /// true, no new alerts can be displayed.
+    /// from trying to play the display/remove alert animations/sfx too many times. 
+    /// While true, no new alerts can be displayed.
     /// </summary>
-    private bool isDisplayingAlert;
+    private bool isDisplayingNotificationAlert;
+
+    /// <summary>
+    /// Used to track when an alert is being displayed. Used mainly to prevent bugs 
+    /// from trying to play the display/remove alert animations/sfx too many times. 
+    /// While true, no new alerts can be displayed.
+    /// </summary>
+    private bool isDisplayingBigWarningAlert;
 
     [Header("SFX")]
     /// <summary>
-    /// The sound effect that plays when the alert is displayed.
+    /// The sound effect that plays when the notification alert is displayed.
     /// </summary>
-    [SerializeField] private AudioSource alertAudio;
+    [SerializeField] private AudioSource notificationAlertAudio;
+
+    /// <summary>
+    /// The sound effect that plays when the big warning alert is displayed.
+    /// </summary>
+    [SerializeField] private AudioSource bigWarningAlertAudio;
 
     [Header("Message Customization")]
     /// <summary>
     /// How long the notification will stay on the screen before being removed.
     /// </summary>
-    [SerializeField] float alertDuration;
+    [SerializeField] float notificationAlertDuration;
 
     /// <summary>
     /// The message that will be displayed to the player.
     /// </summary>
-    [SerializeField] [TextArea] private string alertMessage;
+    [SerializeField] [TextArea] private string notificationAlertMessage;
 
 
     void Update()
@@ -49,7 +66,12 @@ public class Alert : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             // Code to execute when space key is pressed
-            DisplayAlert();
+            DisplayNotificationAlert();
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            // Code to execute when space key is pressed
+            DisplayBigWarningAlert();
         }
     }
 
@@ -57,50 +79,73 @@ public class Alert : MonoBehaviour
     /// Plays the enter transition animation and displays the alert. Starts
     /// a coroutine timer to remove the alert when it expires.
     /// </summary>
-    void DisplayAlert()
+    void DisplayNotificationAlert()
     {
         // Only attempt to display the alert if it is not currently being displayed
-        if(!isDisplayingAlert)
+        if(!isDisplayingNotificationAlert)
         {
-            isDisplayingAlert = true;
-            alertTextUI.text = alertMessage;
-            alertAnimation.Play("DisplayAlert");
-            alertAudio.PlayOneShot(alertAudio.clip);
-            StartCoroutine(WaitForAlertDuration());
-        }
-        else
-        {
-            return;
+            isDisplayingNotificationAlert = true;
+            notificationAlertTextUI.text = notificationAlertMessage;
+            notificationAlertAnimation.Play("DisplayAlert");
+            notificationAlertAudio.PlayOneShot(notificationAlertAudio.clip);
+            StartCoroutine(WaitForNotificationAlertDuration());
         }
     }
 
     /// <summary>
-    /// Coroutine timer that removes the alert when it expires.
+    /// Coroutine timer that removes the notification alert when it expires.
     /// </summary>
-    IEnumerator WaitForAlertDuration()
+    IEnumerator WaitForNotificationAlertDuration()
     {
-        yield return new WaitForSeconds(alertDuration);
-        RemoveAlert();
+        yield return new WaitForSeconds(notificationAlertDuration);
+        RemoveNotificationAlert();
     }
 
     /// <summary>
     /// Plays the exit transition animation that removes the alert.
     /// </summary>
-    void RemoveAlert()
+    void RemoveNotificationAlert()
     {
-        alertAnimation.Play("RemoveAlert");
-        StartCoroutine(WaitForRemoveAnimationDuration());
+        notificationAlertAnimation.Play("RemoveAlert");
+        StartCoroutine(WaitForRemoveNotificationAlertAnimationDuration());
     }
 
     /// <summary>
     /// Waits until the exit transition animation has finished playing and sets
-    /// isDisplayingAlert to false so the next alert can occur.
+    /// isDisplayingNotificationAlert to false so the next alert can occur.
     /// </summary>
-    IEnumerator WaitForRemoveAnimationDuration()
+    IEnumerator WaitForRemoveNotificationAlertAnimationDuration()
     {
-        float removeAnimationDuration = alertAnimation.GetCurrentAnimatorStateInfo(0).length;
+        float removeAnimationDuration = notificationAlertAnimation.GetCurrentAnimatorStateInfo(0).length;
         yield return new WaitForSeconds(removeAnimationDuration);
-        isDisplayingAlert = false;
+        isDisplayingNotificationAlert = false;
+    }
+
+    /// <summary>
+    /// Plays the big warning alert animation
+    /// </summary>
+    void DisplayBigWarningAlert()
+    {
+        if (!isDisplayingBigWarningAlert)
+        {
+            isDisplayingBigWarningAlert = true;
+            bigWarningAlertAnimation.Play("BigWarningAnimation");
+            bigWarningAlertAudio.PlayOneShot(bigWarningAlertAudio.clip);
+            StartCoroutine(WaitForBigWarningAlertDuration());
+        }
+    }
+
+    /// <summary>
+    /// Coroutine timer that removes the big warning alert when it expires.
+    /// </summary>
+    IEnumerator WaitForBigWarningAlertDuration()
+    {
+        // hardcoded big warning animation length because getting it programatically
+        // did not always return the correct duration
+        yield return new WaitForSeconds(4.5f);
+        isDisplayingBigWarningAlert = false;
+        bigWarningAlertAnimation.Play("Empty State");
+
     }
 
 }
