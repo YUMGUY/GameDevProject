9b147c2137df231154ae1e37ea39a1ce8a07b91f
YUMGUY <timothytipham@gmail.com>
2023-04-23 22:57:18 -0500

    Win/Lost Screen, Enemy Flash
    GameManager implemented for win conditions
    - bullet animations starting to be implemented: standard bullet
    - flash dmg script: needs bullet tag

---
+++ Assets/Scripts/Base/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject endScreen;
+    public Image gameOverScreen;
+    public Sprite win;
+    public Sprite lose;
+    public bool conditionMet;
+    public bool coroutineStarted;
+    public bool wonGame;
+    public bool lostGame;
+    public CoreData coreRef;
+    void Start()
+    {
+        gameOverScreen = endScreen.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectWinConditions();
+        if(conditionMet && !coroutineStarted)
+        {
+            StartCoroutine(EndGame());
+            coroutineStarted = true;
+        }
+    }
+
+    public IEnumerator EndGame()
+    {
+        endScreen.SetActive(true);
+        if(wonGame)
+        {
+            gameOverScreen.sprite = win;
+        }
+        else
+        {
+            gameOverScreen.sprite = lose;
+        }
+
+        yield return null;
+    }
+
+    public void DetectWinConditions()
+    {
+        if(coreRef.getEnergy() >= coreRef.getMaxEnergy())
+        {
+            conditionMet = true;
+            wonGame = true;
+        }
+        else if(coreRef.getEnergy() <= 0)
+        {
+            conditionMet = true;
+            lostGame = true;
+        }
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
